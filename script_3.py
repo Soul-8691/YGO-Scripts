a70903d4d586a56f07b12652ec15fd136cbda973
Petuuuhhh <peterswanton191@bellsouth.net>
2024-10-03 21:15:42 -0400

    yep

--- script_3.py
+++ script_3.py
@@ -1678,18 +1678,30 @@ for card in mod:
         pass
     # output.write(card + '\t' + card_info[card]['type'] + '\t' + attribute + '\t' + card_info[card]['race'] + '\t' + str(level) + '\t' + str(atk) + '\t' + str(defn) + '\n')
     # output.write('+#' + card + '#, ' + passw[6:] + passw[4:6] + passw[2:4] + passw[0:2] + '\n')
-    # output.write(desc.replace('\r\n', ' ').replace('\n', ' ')[:478].ljust(478, '�') + '\n')
+    # output.write(desc.replace('"', '\\"').replace('\r\n', ' ').replace('\n', ' ')[:478].ljust(478, '�') + '\n')
     # output.write('+#' + card + '#, (' + str(int(defn/10)) + ' ' + str(int(atk/10)) + ' ' + card_info[card]['type'] + ' ' + card_info[card]['race'] + ' ' + str(level) + ' ' + attribute + ')\n')
     # output.write('#define CARD_NUMBER_' + card.upper().replace(' ', '_') + ' ' + str(counter) + '\n')
+#     output.write(
+# '\t[CARD_NUMBER_' + card.upper().replace(' ', '_') + ''']
+# 	{
+# 		.defense = ''' + str(int(defn/10)) + ''',
+# 		.attack = ''' + str(int(atk/10)) + ''' ,
+# 		.monsterType = MONSTER_TYPE_''' + monster_type.upper().replace(' ', '_') + ''',
+# 		.race = RACE_''' + race.upper().replace(' ', '_') + ''',
+# 		.level = ''' + str(level) + ''',
+# 		.attribute = ATTRIBUTE_''' + attribute.upper().replace(' ', '_') + ''',
+# 	},
+# ''')
+#     output.write(
+# '\t[CARD_NUMBER_' + card.upper().replace(' ', '_').replace('-', '_'.replace('.', '_')) + ''']
+# 	{
+# 		.cardName = "''' + card + '''"
+# 	},
+# ''')
     output.write(
-'\t[CARD_NUMBER_' + card.upper().replace(' ', '_') + ''']
+'\t[CARD_NUMBER_' + card.upper().replace(' ', '_').replace('-', '_'.replace('.', '_')) + ''']
 	{
-		.defense = ''' + str(int(defn/10)) + ''',
-		.attack = ''' + str(int(atk/10)) + ''' ,
-		.monsterType = MONSTER_TYPE_''' + monster_type.upper().replace(' ', '_') + ''',
-		.race = RACE_''' + race.upper().replace(' ', '_') + ''',
-		.level = ''' + str(level) + ''',
-		.attribute = ATTRIBUTE_''' + attribute.upper().replace(' ', '_') + ''',
+		.description = "''' + desc.replace('"', '\\"').replace('\r\n', ' ').replace('\n', ' ')[:478] + '''"
 	},
 ''')
     counter += 1
