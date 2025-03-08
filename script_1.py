aaa256aeda300d9e6ccc87b1ff28f58c6597f170
Petuuuhhh <peterswanton191@bellsouth.net>
2025-03-08 10:38:00 -0500

    Okay

--- script_1.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-f = open("YGOProDeck_Card_Info.json", "w")
-url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
-res = requests.get(url)
-data = json.dumps(res.json(), indent=4)
-f.write(data)
-f.close()
