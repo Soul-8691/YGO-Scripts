6a52e041a7a470a1d42b316adf340419ad3506fa
Petuuuhhh <peterswanton191@bellsouth.net>
2025-03-19 20:49:56 -0400

    Alr

---
+++ pokefirered.py
@@ -0,0 +1,12 @@
+import json
+from set_chronology import set_chronology_ocg
+
+f = open('YGOProDeck_Card_Info.json')
+data = json.load(f)
+card_info = {}
+for card_info_data in data['data']:
+    card_info[card_info_data['name']] = card_info_data
+
+for set in set_chronology_ocg:
+    if len(set_chronology_ocg[set]) == 2:
+        print(set, list(set_chronology_ocg).index(set))
